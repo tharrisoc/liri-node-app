8ba34d797ec6ce6892679ff89b01a74ad52e1c3d
Thomas W Harris <thomaswharris@Thomass-MacBook-Pro.local>
2018-04-07 02:34:58 -0700

    checking in all commands except do-what-it-says working

--- liri.js
+++ liri.js
@@ -3,91 +3,67 @@ var dotenv  = require("dotenv").config();
 var keys    = require("./keys.js");
 var Twitter = require("twit");
 var Spotify = require("node-spotify-api");
-//var apiNode = require("spotify-web-api-node");
-//var apiJs = require("spotify-web-api-js");
-
-var defaultSong  = "The Sign";
-var defaultMovie = "Mr. Nobody";
 
 var spotify = new Spotify(keys.spotify);
 var client  = new Twitter(keys.twitter);
 
+var defaultSong  = "The+Sign";
+var defaultMovie = "Mr.+Nobody";
+
 var command;
+var argument;
+
+var movieName;
+var songName;
+
+var songMatch;
 
 if ( (process.argv[2] !== undefined) && (process.argv[2] !== null)
   && (process.argv[2] !== '') ) {
   command = process.argv[2].toLowerCase().trim();
 } else {
-  console.log( "Usage is: node liri.js <command> <argument>" );
-  console.log( "  Commands: my-tweets  spotify-this-song  movie-this  do-what-it-says" );
-  process.exit();
+  usage();
 }
 
-/* *************Example Code start************************************** */
-/* *************Example Code end**************************************** */
+if ( (command === 'spotify-this-song') || (command === 'movie-this') ) {
+  var numArgs = process.argv.length - 2;
+  if (numArgs === 1) {
+    argument = process.argv[3];
+  } else if (numArgs > 1) {
+    argument = process.argv.slice(3).join('+');
+  } else {
+    argument = '';
+  }
+}
 
 switch (command) {
 
   case 'my-tweets' :
-    var params = {
-                   q: 'BrutusShark',
-                   count: 20
-                 }
-
-    client.get('search/tweets', params, searchedData);
-
-    function searchedData(err, data, response) {
-      var created;
-      var tweet;
-
-      var tweetArray = data.statuses;
-      var tweetCount = tweetArray.length;
-
-      for (var i = 0; i < tweetCount; i++) {
-        created = tweetArray[i].created_at;
-        tweet   = tweetArray[i].text; 
-        console.log(created);
-        console.log(tweet);
-        console.log(" ");
-      }
-    }
-
-    break;
-
-
-  // Actual documentation for node-spotify-api
-  // https://www.npmjs.com/package/node-spotify-api
+      getTweets();
+      break;
 
   case 'spotify-this-song' :
-    var songName;
-    if ( (process.argv[3] !== undefined) && (process.argv[3] !== null)
-      && (process.argv[3] !== '') ) {
-      var songName = process.argv[3].trim();
-    } else {
-      songName = defaultSong;
-    }
+      if ( (argument !== '') && (argument !== undefined) ) {
+        songName = argument;
+      } else {
+        songName = defaultSong;
+        songName = songName.replace('+', ' ');
+      }
 
-    // Note; type is artist OR album OR track
-    spotify.search( { type: 'track', query: songName }, 
-                    function(err, data) {
-                            if (err) {
-                              return console.log("Error occured: " + err);
-                            }
+      songMatch = songName.replace( /\+/g, " ");
 
-                            console.log(data);
-
-                            var objAry = data.tracks.items;
-                            var objCount = objAry.length;
-                            for (i = 0; i < objCount; i++) {
-				console.log(objAry[i]);
-                            }
-                    });
-
-
-    break;
+      searchSpotify();
+      break;
 
   case 'movie-this' :
-    break;
+      if ( (argument !== '') && (argument !== undefined) ) {
+        movieName = argument;
+      } else {
+        movieName = defaultMovie;
+      }
+
+      searchOMDB();
+      break;
 
   case 'do-what-it-says' :
       break;
@@ -96,5 +72,125 @@ switch (command) {
       console.log(command + ' is not a valid command.');
       console.log('Valid commands are: my-tweets  spotify-this-song  movie-this  do-what-it-says');
       process.exit();
-    break;
+      break;
+}
+
+
+// **************************** Tweets **********************************
+function getTweets() {
+  var params = {
+     q: 'BrutusShark',
+     count: 20
+  };
+
+  client.get('search/tweets', params, searchedData);
+}
+
+function searchedData(err, data, response) {
+ var created;
+ var tweet;
+
+ var tweetArray = data.statuses;
+ var tweetCount = tweetArray.length;
+
+ for (var i = 0; i < tweetCount; i++) {
+   created = tweetArray[i].created_at;
+   tweet   = tweetArray[i].text; 
+   console.log(created);
+   console.log(tweet);
+   console.log(" ");
+ }
+}
+
+
+// **************************** Spotify *********************************
+function searchSpotify() {
+  // Note: type is artist OR album OR track
+  spotify.search(
+     { type: 'track', query: songName },
+     function(err, data) {
+       if (err) {
+         // Note: this statement gets executed regardless of whether the
+         //       error occurred on the server, or occurred in this block
+         //       after the data is returned.
+         return console.log("Error occured: " + err);
+       }
+       else {
+	 // data was returned
+	   var objAry = data.tracks.items;
+           var objCount = objAry.length;
+           var numArtists = 0;
+           var artistNames = [];
+           var nameOfSong = '';
+           var previewLink = '';
+           var albumName = '';
+           
+           for ( i = 0; i < objCount; i++ ) {
+             numArtists = objAry[i].artists.length;
+             
+             for ( j = 0; j < numArtists; j++ ) {
+	       artistNames.push(objAry[i].artists[j].name);
+             }
+
+             nameOfSong  = objAry[i].name;
+             previewLink = objAry[i].preview_url;
+             albumName   = objAry[i].album.name;
+
+             // The returned data can include songs whose titles do not even
+             // remotely match the title that the user entered. Try to display
+             // only those songs whose title at least contains the user's
+             // choice as a substring.
+             // TODO: Fix the problem with the regular expression. For now,
+             //       use indexOf. However, this means that the match will
+             //       be case sensitive.
+/*           var result = nameOfSong.search(new RegExp(songName, "i"));
+             if ( result !== -1 ) { */
+
+             var result = nameOfSong.indexOf(songMatch);
+             if ( result !== -1 ) {
+	       // match on title
+	       console.log( "Song: " + nameOfSong );
+               console.log( "Album: " + albumName );
+               console.log( "Artist(s): ");
+               for( k = 0; k < numArtists; k++ ) {
+		 console.log("  " + artistNames[k]);
+               }
+               console.log("Preview: " + previewLink);
+               console.log(" ");
+             }
+           }
+       }  // else !err
+     } // function(err, data()
+  );  // spotify.search()
+} // function searchSpotify()
+
+
+// **************************** OMDB ************************************
+function searchOMDB() {
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  request( queryUrl, 
+     function(error, response, body) {
+       if( error ) {
+	 return console.log(error);
+       }
+
+       // If the request is successful
+       if ( response.statusCode === 200 ) {
+         console.log("Title: " + JSON.parse(body).Title);
+         console.log("Release Year: " + JSON.parse(body).Year);
+         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+         console.log("Country: " + JSON.parse(body).Country);
+         console.log("Language: " + JSON.parse(body).Language);
+         console.log("Plot: " + JSON.parse(body).Plot);
+         console.log("Actors: " + JSON.parse(body).Actors);
+      }
+   });
+}
+
+// **************************** Usage ***********************************
+function usage() {
+  console.log( "Usage is: node liri.js <command> <argument>" );
+  console.log( "  Commands: my-tweets  spotify-this-song  movie-this  do-what-it-says" );
+  process.exit();
 }
